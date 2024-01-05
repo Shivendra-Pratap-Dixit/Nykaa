e32d93aa29609f11a92969abd8233d427b1ec254
shivendra_pratap_dixit <cvendradixit@gmail.com>
2024-01-05 22:59:29 +0530

    Maine to khatti toffee khai hai

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div><h1>Welcome To Nykaa You Can Login For 
-        Admin Dashboard </h1></div>
+    <div className='flex flex-col my-4 justify-center items-center'><h1 className='font-poppins text-lg my-4'>Welcome To Nykaa You Can Login For 
+        Admin Dashboard </h1>
+        <img className='border rounded-xl' src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/ca623e133073675.61b61c12125e5.png' alt='nykaa'/>
+        </div>
   )
 }
 
